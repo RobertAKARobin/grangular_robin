d9af826f58471fc37f41e0babfd37058c001b3f5
RobertAKARobin <robertgfthomas@gmail.com>
2015-09-04 18:14:07 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,84 @@
+(function(){
+  angular
+    .module("Grumblr", []);
+
+})();
+
+(function(){
+  angular
+    .module("Grumblr")
+    .factory("API", ["$http", API]);
+
+  function API($http){
+    var baseURL = "http://grumblr.wdidc.org/";
+    return {
+      get: function(endpoint){
+        return $http.get(baseURL + endpoint);
+      },
+      post: function(endpoint){
+        return $http.post(baseURL + endpoint);
+      },
+      put: function(endpoint){
+
+      }
+    }
+  }
+})();
+
+(function(){
+  angular
+    .module("Grumblr")
+    .factory("Comment", ["API", Comment]);
+
+  function Comment($api){
+
+  }
+})();
+
+(function(){
+  angular
+    .module("Grumblr")
+    .factory("Grumble", ["API", Grumble]);
+
+  function Grumble($api){
+    return function(input){
+      var self = this;
+      self.comments = [];
+      self.getComments = getComments;
+      angular.extend(self, input);
+      self.getComments();
+    }
+
+    function getComments(){
+      var self = this;
+      $api
+        .get("grumbles/" + this.id + "/comments")
+        .then(function(response){
+          self.comments = response.data;
+        });
+    }
+  }
+})();
+
+(function(){
+  angular
+    .module("Grumblr")
+    .controller("GrumblesController", ["API", "Grumble", GrumblesController]);
+
+  function GrumblesController($api, Grumble){
+    var self = {};
+    self.all = [];
+    self.getAll = getAll;
+    self.getAll();
+    return self;
+
+    function getAll(){
+      return $api.get("grumbles").then(function(response){
+        var x;
+        for(x = response.data.length - 1; x > 0; x--){
+          self.all.push(new Grumble(response.data[x]));
+        }
+      });
+    }
+  }
+})();
