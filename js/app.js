fc38e41c4ddd14ea8fee96dd03511196936adf21
RobertAKARobin <robertgfthomas@gmail.com>
2015-09-05 00:23:07 -0400

    nice layout

--- js/app.js
+++ js/app.js
@@ -45,19 +45,28 @@
       var self = this;
       self.comments = [];
       self.getComments = getComments;
+      self.toggleEditing = toggleEditing;
       angular.extend(self, input);
       self.getComments();
+    }
+
+    function toggleEditing(){
+      this.editing = this.editing ? false : true;
     }
 
     function getComments(){
       var self = this;
       $api
-        .get("grumbles/" + this.id + "/comments")
+        .get("grumbles/" + self.id + "/comments")
         .then(function(response){
           self.comments = response.data;
         });
     }
   }
+})();
+
+(function(){
+
 })();
 
 (function(){
